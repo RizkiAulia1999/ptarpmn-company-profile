e8cda4e2194e382375cda06b6c62f36633501a94
RizkiAulia1999 <auliarizki0970@gmail.com>
2026-04-24 09:29:34 +0700

    update link css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,8 +16,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Source+Sans+3:wght@300;400;600&display=swap" rel="stylesheet" />
 
     <!-- Template Main CSS File -->
-    <link rel="stylesheet" href="{{ asset('/asset/css/style.css') }}?v={{ time() }}">
-
+    <link href="{{ asset('/asset/css/style.css') }}" rel="stylesheet" />
 </head>
 
 <body>
