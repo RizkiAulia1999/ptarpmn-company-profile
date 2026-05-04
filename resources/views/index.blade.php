230233691df143f5b2d6fb830242a966cf764077
RizkiAulia1999 <auliarizki0970@gmail.com>
2026-05-04 11:09:54 +0700

    Ubah Link CSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,8 +16,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Source+Sans+3:wght@300;400;600&display=swap" rel="stylesheet" />
 
     <!-- Template Main CSS File -->
-    <link href="{{ asset('/asset/css/style.css') }}" rel="stylesheet" />
-    <link href="{{ asset('/asset/css/beranda.css') }}" rel="stylesheet" />
+    <link href="{{ asset('asset/css/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('asset/css/beranda.css') }}" rel="stylesheet" />
 
 </head>
 
